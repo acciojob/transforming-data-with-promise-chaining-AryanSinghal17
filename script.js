58fd9f12a323b40adcbb248398f5920b482901ee
Aryan Singhal <115396133+AryanSinghal17@users.noreply.github.com>
2026-09-20 14:30:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,60 @@
-//your JS code here. If required.
+let btn = document.getElementById('btn');
+let num = document.getElementById('ip');
+let output = document.getElementById('output');
+
+btn.addEventListener('click', () => {
+   return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(num.value);
+    }, 2000);
+  })
+
+  .then((result) => {
+    output.innerText = `Result: ${result}`;
+    return new Promise((resolve,reject)=>{
+      setTimeout(() => {
+      resolve(result);
+    }, 2000);
+    })
+  })
+
+  .then((result)=>{
+    result = result*2;
+    output.innerText = `Result ${result}`;
+    return new Promise((resolve,reject)=>{
+      setTimeout(()=>{
+        resolve(result);
+      },1000)
+    })
+  })
+
+  .then((result)=>{
+    result = result - 3;
+    output.innerText = `Result ${result}`;
+    return new Promise((resolve,reject)=>{
+      setTimeout(()=>{
+        resolve(result);
+      },1000)
+    })
+  })
+
+  .then((result)=>{
+    result = result/2;
+    output.innerText = `Result ${result}`;
+    return new Promise((resolve,reject)=>{
+      setTimeout(()=>{
+        resolve(result);
+      },1000)
+    })
+  })
+
+  .then((result)=>{
+    result = result+10;
+    output.innerText = `Result ${result}`;
+    return new Promise((resolve,reject)=>{
+      setTimeout(()=>{
+        resolve(result);
+      },1000)
+    })
+  })
+});
